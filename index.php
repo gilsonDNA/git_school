4fe7f5f241faab25f593ac0e58b3fd87307f4c9e
Gilson Araujo <gilso@idnadevendas.com.br>
2014-11-08 19:07:08 -0200

    Commit teste

--- index.php
+++ index.php
@@ -8,3 +8,14 @@
 
  echo "Olá School of Net";
 
+$num1 = 10;
+$num2 = 30;
+
+try{
+    $result = ($num1 + $num2) / 2;
+    echo "Resultado = ".$result;
+}catch (Exception $e){
+    echo "Número inválido! Num1= ".$num1." Num2= ".$num2;
+
+}
+
